0bc1d2cbe8b91b4770d7c4709210daf2b499499d
Lucho Zelada <luisdanger96@gmail.com>
2022-05-29 17:39:50 -0500

    ADD:function get movies

--- scripts/services/movies_services.js
+++ scripts/services/movies_services.js
@@ -1,5 +1,7 @@
-import BASE_URI from "../config/config"
+import BASE_URI, { api_key } from "../config/config.js"
 
-export function getMovies() {
-    fetch()
+
+export  async function getMovies() {
+    const response = await fetch(`${BASE_URI}/popular${api_key}`)
+    return response;
 }
