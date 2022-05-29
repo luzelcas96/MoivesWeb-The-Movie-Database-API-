ffd4b3acaaa7b287fce69893d9b6e84b475bb74d
Lucho Zelada <luisdanger96@gmail.com>
2022-05-29 17:56:42 -0500

    ADD: movies and movie details

--- scripts/services/movies_services.js
+++ scripts/services/movies_services.js
@@ -1,12 +1,25 @@
-import BASE_URI, { api_key } from "../config/config.js"
+import{ BASE_URI, api_key } from "../config/config.js"
 
 
-export  async function getMovies() {
+async function validResponse(response) {
+    let body;
+    try {
+        body = await response.json();
+    } catch (error) {
+        body = response.statusText;
+    }
+}
+
+export async function popularMovies() {
     const response = await fetch(`${BASE_URI}/popular${api_key}`)
-    return response;
+    const movies = await validResponse(response);
+    return movies;
 }
 
 
 export async function moviesDetails(id) {
-    const detailsMovies = await fetch(`${BASE_URI}/${id}${api_key}`);
+    const response = await fetch(`${BASE_URI}/${id}${api_key}`);
+    const movie = await validResponse(response);
+    return movie;
 }
+
